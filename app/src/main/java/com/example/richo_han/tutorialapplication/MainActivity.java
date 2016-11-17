254bb1766c6b294dbb471a635940f87268558e1e
Richo_Han <Richo_Han@asus.com>
2016-11-17 12:01:51 +0800

    Add Overview activity.

--- app/src/main/java/com/example/richo_han/tutorialapplication/MainActivity.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.richo_han.tutorialapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity{
     public final static String EXTRA_NAME = "com.example.tutorialapp.NAME";
@@ -12,5 +14,10 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startOverview(View view) {
+        Intent intent = new Intent(this, OverviewActivity.class);
+        startActivity(intent);
     }
 }
