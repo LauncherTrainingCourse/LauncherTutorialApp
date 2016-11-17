af9512a2b7a994ef07d8f41355c96007cf28c143
Richo_Han <Richo_Han@asus.com>
2016-11-17 14:37:56 +0800

    Add menu to toolbar.

--- app/src/main/java/com/example/richo_han/tutorialapplication/OverviewActivity.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/OverviewActivity.java
@@ -2,12 +2,40 @@ package com.example.richo_han.tutorialapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class OverviewActivity extends AppCompatActivity {
+
+    public final static String TAG = OverviewActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.overview_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_overview, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                Log.i(TAG, "Settings button pushed.");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
