ce086036c77f4861dc246ce81cbb036dba8a80f8
Richo_Han <Richo_Han@asus.com>
2016-11-17 09:45:58 +0800

    Create a new toolbar with menu.

--- app/src/main/java/com/example/richo_han/tutorialapplication/MainActivity.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/MainActivity.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,7 +21,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     public final static String EXTRA_NAME = "com.example.tutorialapp.NAME";
     public final static String EXTRA_GENDER = "com.example.tutorialapp.GENDER";
     public final static String EXTRA_EMAIL = "com.example.tutorialapp.EMAIL";
-    private static final String TAG = "MainActivity";
     private String gender = "";
     private String domain = "";
 
@@ -25,7 +28,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Form");
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
 
         Spinner spinner = (Spinner) findViewById(R.id.domain_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.domain_array, android.R.layout.simple_spinner_dropdown_item);
@@ -167,4 +172,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
      * touch is activated or when the adapter becomes empty. */
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_favorite:
+                Log.i("Toolbar", "Favorite button pushed.");
+                return true;
+            case R.id.action_settings:
+                Log.i("Toolbar", "Settings button pushed.");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
