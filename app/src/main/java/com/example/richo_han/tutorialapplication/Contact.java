506fa990911820c13c8f06b11e1bae40e91fb27b
Richo_Han <Richo_Han@asus.com>
2016-11-18 17:42:04 +0800

    Add ListView to Contact page.

---
+++ app/src/main/java/com/example/richo_han/tutorialapplication/Contact.java
@@ -0,0 +1,13 @@
+package com.example.richo_han.tutorialapplication;
+
+/**
+ * Created by Richo_Han on 2016/11/18.
+ */
+
+public class Contact {
+    public String name;
+
+    public Contact(String name){
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/richo_han/tutorialapplication/ContactPageFragment.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/ContactPageFragment.java
@@ -6,22 +6,35 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ContactPageFragment extends Fragment {
     public static final String ARG_PAGE = "ARG_PAGE";
-
+    public ContactAdapter contactAdapter;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        ArrayList<Contact> arrayOfContacts = new ArrayList<Contact>();
+        contactAdapter = new ContactAdapter(this.getContext(), arrayOfContacts);
+
+        /* Deal with data binding here.
+        contactAdapter.add(new Contact("Richo"));
+        contactAdapter.add(new Contact("John"));
+        contactAdapter.add(new Contact("Jack"));
+        */
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_contact_page, container, false);
-        TextView textView = (TextView) view;
-        textView.setText("Contact Page");
+
+        ListView listView = (ListView) view.findViewById(R.id.lv_contact);
+        listView.setAdapter(contactAdapter);
         return view;
     }
 
