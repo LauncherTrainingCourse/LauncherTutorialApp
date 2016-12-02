73d3a4f2fc0fcd8b418e705d724ea418126f185c
Richo_Han <Richo_Han@asus.com>
2016-12-02 15:55:48 +0800

    Add library that help db debugging: http://facebook.github.io/stetho/

--- app/src/main/java/com/example/richo_han/tutorialapplication/MainActivity.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.facebook.stetho.Stetho;
+
 public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Stetho.initializeWithDefaults(this);
     }
 
     /***
