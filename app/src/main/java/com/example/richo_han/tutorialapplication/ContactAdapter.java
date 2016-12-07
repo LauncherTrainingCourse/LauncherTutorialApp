62abaecc51373b6f66fac068110d2dd96c277c37
Richo_Han <Richo_Han@asus.com>
2016-12-07 17:57:43 +0800

    Code refactoring.

--- app/src/main/java/com/example/richo_han/tutorialapplication/ContactAdapter.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/ContactAdapter.java
@@ -1,8 +1,6 @@
 package com.example.richo_han.tutorialapplication;
 
 import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
