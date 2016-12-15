7cb6b97677a00153e51c23c68ef84e5e615ba6d0
Richo_Han <Richo_Han@asus.com>
2016-12-15 11:51:05 +0800

    Receive and show the title of the news.

--- app/src/main/java/com/example/richo_han/tutorialapplication/ShareActivity.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/ShareActivity.java
@@ -1,7 +1,9 @@
 package com.example.richo_han.tutorialapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShareActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class ShareActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra(Intent.EXTRA_SUBJECT);
+
+        ((TextView) (findViewById(R.id.news_title))).setText(title);
     }
 }
