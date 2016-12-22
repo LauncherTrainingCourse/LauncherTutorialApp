420397fb69103185e922e56289496bbdc86eb114
Richo_Han <Richo_Han@asus.com>
2016-12-22 13:12:42 +0800

    Add edit button.

--- app/src/main/java/com/example/richo_han/tutorialapplication/OverviewActivity.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/OverviewActivity.java
@@ -1,6 +1,10 @@
 package com.example.richo_han.tutorialapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
