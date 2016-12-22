1969b915609de11f5f60b9f9bfbe8e2b61b316f5
Richo_Han <Richo_Han@asus.com>
2016-12-22 14:15:56 +0800

    Call SEND action.

--- app/src/main/java/com/example/richo_han/tutorialapplication/ContactInfoFragment.java
+++ app/src/main/java/com/example/richo_han/tutorialapplication/ContactInfoFragment.java
@@ -10,6 +10,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.clans.fab.FloatingActionButton;
+
 
 public class ContactInfoFragment extends Fragment {
     public Contact contact;
@@ -43,7 +45,31 @@ public class ContactInfoFragment extends Fragment {
         tvCompany.setText(contact.company);
         tvEmail.setText(contact.email);
 
+        FloatingActionButton editFab = (FloatingActionButton) view.findViewById(R.id.edit_fab);
+        editFab.setOnClickListener(clickListener);
+
         return view;
     }
 
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.edit_fab:
+                    Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                    shareIntent.setType("text/plain");
+                    shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Title");
+                    shareIntent.putExtra(Intent.EXTRA_TEXT, "Text");
+
+                    String title = "Title";
+
+                    if(shareIntent.resolveActivity(getActivity().getPackageManager()) != null){
+                        startActivity(Intent.createChooser(shareIntent, title));
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 }
